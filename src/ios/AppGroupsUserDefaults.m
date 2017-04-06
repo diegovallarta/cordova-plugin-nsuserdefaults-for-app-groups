565880160d15efa0291bd23eba8a45b35cfd0bcd
Diego Vallarta <diego.vallarta@gmail.com>
2017-04-06 15:23:30 +0200

    run load and save methods in background threads

--- src/ios/AppGroupsUserDefaults.m
+++ src/ios/AppGroupsUserDefaults.m
@@ -12,20 +12,24 @@
     NSString* value = [arguments objectForKey:@"value"];
     NSString* suite = [arguments objectForKey:@"suite"];
 
-    // do the magic
-    NSUserDefaults *prefs = [[NSUserDefaults alloc] initWithSuiteName:suite];
-    [prefs setObject:value forKey:key];
-    [prefs synchronize];
+    [self.commandDelegate runInBackground:^{
 
-    // give the callback
-    CDVPluginResult* result = nil;
-    if([[prefs stringForKey:key] isEqualToString:value])
-    {
-      result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-    } else {
-      result = [CDVPluginResult resultWithStatus:CDVCommandStatus_IO_EXCEPTION];
-    }
-    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+        // do the magic
+        NSUserDefaults *prefs = [[NSUserDefaults alloc] initWithSuiteName:suite];
+        [prefs setObject:value forKey:key];
+        [prefs synchronize];
+
+        // give the callback
+        CDVPluginResult* result = nil;
+        if([[prefs stringForKey:key] isEqualToString:value])
+        {
+            result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+        } else {
+            result = [CDVPluginResult resultWithStatus:CDVCommandStatus_IO_EXCEPTION];
+        }
+        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+
+    }];
 }
 
 - (void) saveAll:(CDVInvokedUrlCommand*)command
@@ -116,19 +120,21 @@
     NSString* key = [arguments objectForKey:@"key"];
     NSString* suite = [arguments objectForKey:@"suite"];
 
-    // do more magic
-    NSUserDefaults *prefs = [[NSUserDefaults alloc] initWithSuiteName:suite];
-    NSString* callbackResult = [prefs stringForKey:key];
+    [self.commandDelegate runInBackground:^{
+        // do more magic
+        NSUserDefaults *prefs = [[NSUserDefaults alloc] initWithSuiteName:suite];
+        NSString* callbackResult = [prefs stringForKey:key];
 
-    // give the callback
-    CDVPluginResult* result = nil;
-    if(callbackResult != nil)
-    {
-      result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:callbackResult];
-    } else {
-      result = [CDVPluginResult resultWithStatus:CDVCommandStatus_IO_EXCEPTION];
-    }
-    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+        // give the callback
+        CDVPluginResult* result = nil;
+        if(callbackResult != nil)
+        {
+            result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:callbackResult];
+        } else {
+            result = [CDVPluginResult resultWithStatus:CDVCommandStatus_IO_EXCEPTION];
+        }
+        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+    }];
 }
 
 @end
